4331132e33f76c2079e139e29d5297a5e093a609
itsMontoya <josh@montoya.io>
2017-05-23 16:10:57 -0700

    Better commenting for utilities.go

--- utilities.go
+++ utilities.go
@@ -1,45 +1,57 @@
 package turtle
 
+// store is a basic data store
 type store map[string]Value
 
+// get will retrieve a value for a provided key
 func (s store) get(key string) (value Value, err error) {
 	var ok bool
 	if value, ok = s[key]; !ok {
+		// Value does not exist for this key
 		err = ErrKeyDoesNotExist
 	}
 
 	return
 }
 
+// exists will return a boolean representing if a value exists for a provided key
 func (s store) exists(key string) (ok bool) {
 	_, ok = s[key]
 	return
 }
 
+// txnStore is a specialized data store handling transaction actions
 type txnStore map[string]*action
 
+// get will retrieve a value for a provided key
 func (t txnStore) get(key string) (value Value, ok bool, err error) {
 	var a *action
 	if a, ok = t[key]; !ok {
+		// No actions were taken for this key during the transaction
 		return
 	}
 
 	if !a.put {
+		// Key was deleted during this transaction, return early with error
 		err = ErrKeyDoesNotExist
 		return
 	}
 
+	// Key was updated during the transaction, set value
 	value = a.value
 	return
 }
 
+// exists will return a boolean representing if an action was taken for a provided key
 func (t txnStore) exists(key string) (ok bool) {
 	_, ok = t[key]
 	return
 }
 
 type action struct {
-	put   bool
+	// put state, false assumes a delete action
+	put bool
+	// value of action, only looked at during put state
 	value Value
 }
 
@@ -47,9 +59,13 @@ type action struct {
 type Txn interface {
 	clear()
 
+	// Get value by key
 	Get(key string) (Value, error)
+	// Put value by key
 	Put(key string, value Value) error
+	// Delete key
 	Delete(key string) error
+	// ForEach key/value pair
 	ForEach(fn ForEachFn) error
 }
 
