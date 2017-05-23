35998027a5b8fce3fcc5c8508e7b5eadd95e7241
itsMontoya <josh@montoya.io>
2017-05-23 16:00:17 -0700

    Some minor cleanup, adding more thorough commenting

--- turtle.go
+++ turtle.go
@@ -54,47 +54,71 @@ type Turtle struct {
 	closed uint32
 }
 
+// isClosed will atomically check the closed state of the database
 func (t *Turtle) isClosed() bool {
 	return atomic.LoadUint32(&t.closed) == 1
 }
 
+// load is called on DB initialization and will populate the in-memory store from our file back-end
 func (t *Turtle) load() (err error) {
+	// Inner error, this is intended so that the error returned by ForEach
+	// does not overwrite a true error we encounter during iteration.
+	// To explain further - if ForEach returns a nil error, yet we encountered
+	// an unmarshal error during the loop. The error would be returned as nil.
 	var ierr error
 	if err = t.mrT.ForEach(func(lineType byte, key, value []byte) (end bool) {
 		if lineType == mrT.DeleteLine {
+			// We encountered a delete line, remove the key from the map and return early
 			delete(t.s, string(key))
 			return
 		}
 
 		var v Value
 		if v, ierr = t.ufn(value); err != nil {
+			// Error encountered while unmarshaling, return and end the loop early
 			return true
 		}
 
+		// Set the key as our parsed value within the database store
 		t.s[string(key)] = v
 		return
 	}); err != nil {
+		// Error encountered during ForEach, generally a disk or middleware related issue
+		// Any error which may be encountered SHOULD occur before any iteration occurs
+		// TODO: Do some heavy combing through the codebase to confirm this statement
 		return
 	}
 
+	// Return any inner errors encountered
 	return ierr
 }
 
 func (t *Turtle) snapshot() (errs *errors.ErrorList) {
+	// Acquire read-lock
 	t.mux.RLock()
+	// Defer release of read-lock
 	defer t.mux.RUnlock()
 
 	errs.Push(t.mrT.Archive(func(txn *mrT.Txn) (err error) {
+		// Iterate through all items
 		for key, value := range t.s {
 			var b []byte
+			// Marshal the value as bytes
 			if b, err = t.mfn(value); err != nil {
 				errs.Push(err)
 				err = nil
-				// We don't necessarily need to stop the world for marshal errors, add to errors list and move on
+				// We don't necessarily need to stop the world for marshal errors,
+				// add to errors list and move on
 				continue
 			}
 
+			// Put the updated bytes to the back-end
 			if err = txn.Put([]byte(key), b); err != nil {
+				// Errors on put are something we need to immediately yield for.
+				// The only possible errors we would encounter are:
+				//		1. Disk issues
+				//		2. Middleware issues
+				// Both of which would occur for every subsequent item
 				return
 			}
 		}
@@ -109,9 +133,11 @@ func (t *Turtle) Read(fn TxnFn) (err error) {
 	var txn RTxn
 	// Acquire read-lock
 	t.mux.RLock()
-	// Defer read-lock release
+	// Defer release of read-lock
 	defer t.mux.RUnlock()
+
 	if t.isClosed() {
+		// DB is closed and we cannot perform any actions, return with error
 		return errors.ErrIsClosed
 	}
 
@@ -119,6 +145,7 @@ func (t *Turtle) Read(fn TxnFn) (err error) {
 	txn.s = t.s
 	// Defer txn clear
 	defer txn.clear()
+
 	// Call provided func
 	return fn(&txn)
 }
@@ -128,9 +155,11 @@ func (t *Turtle) Update(fn TxnFn) (err error) {
 	var txn WTxn
 	// Acquire write-lock
 	t.mux.Lock()
-	// Defer write-lock release
+	// Defer release of write-lock
 	defer t.mux.Unlock()
+
 	if t.isClosed() {
+		// DB is closed and we cannot perform any actions, return with error
 		return errors.ErrIsClosed
 	}
 
@@ -142,6 +171,7 @@ func (t *Turtle) Update(fn TxnFn) (err error) {
 	txn.mfn = t.mfn
 	// Defer txn clear
 	defer txn.clear()
+
 	// Call provided func
 	if err = fn(&txn); err != nil {
 		return
@@ -158,11 +188,14 @@ func (t *Turtle) Update(fn TxnFn) (err error) {
 // Close will close turtle
 func (t *Turtle) Close() (err error) {
 	if !atomic.CompareAndSwapUint32(&t.closed, 0, 1) {
+		// DB is already closed, return with error
 		return errors.ErrIsClosed
 	}
 
 	var errs errors.ErrorList
+	// Attempt to snapshot
 	errs.Push(t.snapshot())
+	// Close file back-end
 	errs.Push(t.mrT.Close())
 	return errs.Err()
 }
