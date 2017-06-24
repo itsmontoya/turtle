5691304ff87bc03e4868ef20d2a5b962a078fbf6
itsMontoya <josh@montoya.io>
2017-06-23 18:02:23 -0700

    adding generic interface type

--- types/types.go
+++ types/types.go
@@ -1,3 +1,4 @@
 package types
 
 //go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/. -out=bytes/turtle.go gen "Value=[]byte"
+//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/. -out=generic/. gen "Value=interface{}"
