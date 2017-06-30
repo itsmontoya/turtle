229aaae27ad7b2cb8bad50c8f9d9d706a148cce8
itsMontoya <josh@montoya.io>
2017-06-30 16:50:49 -0700

    Making bucket test more thorough

--- bucket_test.go
+++ bucket_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 func TestBucket(t *testing.T) {
-	var err error
+	var (
+		val Value
+		err error
+	)
+
 	b := newBucket()
 
 	if _, err = b.Get("0"); err == nil {
@@ -14,5 +18,29 @@ func TestBucket(t *testing.T) {
 
 	if err = b.Put("0", "hello_world"); err == nil {
 		t.Fatal("error not encountered when expected")
+	}
+
+	if err = b.Delete("0"); err == nil {
+		t.Fatal("error not encountered when expected")
+	}
+
+	b.put("0", "hello_world")
+
+	if val, err = b.Get("0"); err != nil {
+		t.Fatal(err)
+	}
+
+	if str := val.(string); str != "hello_world" {
+		t.Fatalf("invalid value, expected %s and received %s", "hello_world", str)
+	}
+
+	var cnt int
+	b.ForEach(func(key string, val Value) bool {
+		cnt++
+		return false
+	})
+
+	if cnt != 1 {
+		t.Fatalf("invalid count, expected %d and received %d", 1, cnt)
 	}
 }
