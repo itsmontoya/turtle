627520d03443d74dae009ba02b0a65eb72e26d32
itsMontoya <josh@montoya.io>
2017-07-06 12:08:27 -0700

    Forgot to update the repo reference

--- types/types.go
+++ types/types.go
@@ -1,10 +1,10 @@
 package types
 
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/bucket.go -out=bytes/bucket.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/buckets.go -out=bytes/buckets.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/rtxn.go -out=bytes/rtxn.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/turtle.go -out=bytes/turtle.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/txnBucket.go -out=bytes/txnBucket.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/txnBuckets.go -out=bytes/txnBuckets.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/utilities.go -out=bytes/utilities.go gen "Value=[]byte"
-//go:generate genny -pkg bytes -in=$HOME/go/src/github.com/itsmontoya/turtle/wtxn.go -out=bytes/wtxn.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/bucket.go -out=bytes/bucket.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/buckets.go -out=bytes/buckets.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/rtxn.go -out=bytes/rtxn.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/turtle.go -out=bytes/turtle.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/txnBucket.go -out=bytes/txnBucket.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/txnBuckets.go -out=bytes/txnBuckets.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/utilities.go -out=bytes/utilities.go gen "Value=[]byte"
+//go:generate genny -pkg turtleDB -in=$HOME/go/src/github.com/Path94/turtle/wtxn.go -out=bytes/wtxn.go gen "Value=[]byte"
